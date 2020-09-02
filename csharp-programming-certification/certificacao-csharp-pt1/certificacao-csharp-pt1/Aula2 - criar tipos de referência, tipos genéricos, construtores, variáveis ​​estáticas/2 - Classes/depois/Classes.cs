6dbbb864675566cdbb5700a04ef93412a4c22f8d
lucasluchi <lds.lucasluchi@gmail.com>
2020-09-02 09:09:32 -0300

    first commit

---
+++ csharp-programming-certification/certificacao-csharp-pt1/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/depois/Classes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace certificacao_csharp_roteiro
+{
+    class Classes : IAulaItem
+    {
+        public void Executar()
+        {
+            ClassePosicaoGPS posicao1 = new ClassePosicaoGPS()
+            { Latitude = 13.78, Longitude = 29.51 };
+
+            posicao1 = new ClassePosicaoGPS(13.78, 29.51);
+
+            Console.WriteLine(posicao1);
+
+            PosicaoGPSComLeitura posicao2 = new PosicaoGPSComLeitura(13.78, 29.51, DateTime.Now);
+
+            Console.WriteLine(posicao2);
+        }
+    }
+
+    class ClassePosicaoGPS : IGPS
+    {
+        public double Latitude;
+        public double Longitude;
+
+
+        public ClassePosicaoGPS()
+        {
+
+        }
+
+        public ClassePosicaoGPS(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public bool EstaNoHemisferioNorte()
+        {
+            return Latitude > 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}";
+        }
+    }
+
+    class PosicaoGPSComLeitura : ClassePosicaoGPS
+    {
+        private readonly DateTime dataLeitura;
+
+        public PosicaoGPSComLeitura(double latitude, double longitude, DateTime dataLeitura) : base(latitude, longitude)
+        {
+            this.dataLeitura = dataLeitura;
+        }
+
+        public override string ToString()
+        {
+            return $"Latitude: {Latitude}, Longitude: {Longitude}, Data e Hora: {dataLeitura}";
+        }
+    }
+}
